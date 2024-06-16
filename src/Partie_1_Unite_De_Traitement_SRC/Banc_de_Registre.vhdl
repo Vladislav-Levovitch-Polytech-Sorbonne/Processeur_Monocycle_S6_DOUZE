b19982fee671707519ddd542c76fcf1c36aebab7
Vladislav-Levovitch-Polytech-Sorbonne <vladislav.balayan@etu.sorbonne-universite.fr>
2024-06-16 19:38:41 +0200

    Banc de Registre 1

--- src/Partie_1_Unite_De_Traitement_SRC/Banc_de_Registre.vhdl
+++ src/Partie_1_Unite_De_Traitement_SRC/Banc_de_Registre.vhdl
@@ -19,15 +19,51 @@ end Banc_de_Registre_entity;
 
 architecture Banc_de_Registre_architecture of Banc_de_Registre_entity is
 -- Signal
-signal S_SIGNAL_UAL : std_logic_vector(31 downto 0) := (others => '0');
+signal S_SIGNAL_RB : std_logic_vector(31 downto 0) := (others => '0');
 
+
+signal Registre_0_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_1_RB : std_logic_vector(31 downto 0) := (others => '0'); -- Choix personnel, je prefere 16 variables a un tableau
+signal Registre_2_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_3_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_4_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_5_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_6_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_7_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_8_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_9_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_10_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_11_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_12_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_13_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_14_RB : std_logic_vector(31 downto 0) := (others => '0');
+signal Registre_15_RB : std_logic_vector(31 downto 0) := (others => '0');
+
+
+-- Initialisation des sorties
+A_BR <= (others => '0');
+B_BR <= (others => '0');
 -- Content
 begin
     S_UAL <= S_SIGNAL_UAL;
 
-    process (Ra_BR,Rb_BR,Rw_BR, W_BR) -- Combinatoire lecture    -- Je ne suis pas sur qu un petit if Enable n aurait pas ete de trop
+    process (Ra_BR,Rb_BR) -- Combinatoire lecture    -- Je ne suis pas sur qu un petit if Enable n aurait pas ete de trop
     begin
-    
+        case Ra_BR is 
+            when "0000" =>  A_BR <= Registre_0_RB;
+            when "0000" =>  A_BR <= Registre_0_RB;
+            when "0000" =>  A_BR <= Registre_0_RB;
+            when "0000" =>  A_BR <= Registre_0_RB;
+            when "0000" =>  A_BR <= Registre_0_RB;
+            when "0000" =>  A_BR <= Registre_0_RB;
+            when "0000" =>  A_BR <= Registre_0_RB;
+            when "0000" =>  A_BR <= Registre_0_RB;
+            when "0000" =>  A_BR <= Registre_0_RB;
+            when "0000" =>  A_BR <= Registre_0_RB;
+            when "0000" =>  A_BR <= Registre_0_RB;
+            
+
+
     end process;
 
     process (Clk_BR,Rst_BR,) -- Synchrone Ecriture 
